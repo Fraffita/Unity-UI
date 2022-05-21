1c64d6ae783e3dc69ff89bdaacca57660acb378b
Franco Raffaele <fran.raffa15@gmail.com>
2022-05-21 12:57:57 -0300

    Videos del 4 al 7 hechos
    Colocación de input Field, botón, y validador de contraseña.

--- Assets/Scripts/MayorDeEdad.cs
+++ Assets/Scripts/MayorDeEdad.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MayorDeEdad : MonoBehaviour
 {
     public int edadUsuario;
+    public Text texto;
 
     void Start()
     {
        if (edadUsuario >= 18)
         {
+            texto.text = "Es mayor de edad";
             Debug.Log("Es mayor de edad");
         } 
 
        else
         {
+            texto.text = "No es mayor de edad";
             Debug.Log("No es mayor de edad");
         }
     }
---
+++ Assets/Scripts/ValidarContraseña.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+    public Text textoMsj;
+    public GameObject cartelMsj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseniaCorrecta = "12345";
+        cartelMsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContrasenia()
+    {
+        contraseniaUsuario = ingresoUsuario.text;
+
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            cartelMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelMsj.SetActive(true);
+            textoMsj.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
