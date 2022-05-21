bd9a072f57ae3868285bfa7ce4eab5e041090de3
Franco Raffaele <fran.raffa15@gmail.com>
2022-05-20 21:28:11 -0300

    video 1 terminado
    script asociado a Main Camera y escena creada como MayoríaDeEdad.

---
+++ Assets/Scripts/MayorDeEdad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MayorDeEdad : MonoBehaviour
+{
+    public int edadUsuario;
+
+    void Start()
+    {
+       if (edadUsuario >= 18)
+        {
+            Debug.Log("Es mayor de edad");
+        } 
+
+       else
+        {
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+   
+}
